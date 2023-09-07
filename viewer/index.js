22ac2b46edf8892e1b51adf5380ae417ed76ff24
Nicolas van Kempen <nvankemp@gmail.com>
2023-09-07 10:05:19 -0400

    Draw squares

--- viewer/index.js
+++ viewer/index.js
@@ -1,17 +1,88 @@
-(function () {
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
+(async function () {
+    // This should match with the Event enum class in C++.
+    const Event = {
+        Allocation: 1n,
+        Free: 2n,
+        Checkpoint: 3n,
+    };
 
-    const render = function () {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
 
-        context.fillStyle = '#000';
-        context.fillRect(0, 0, canvas.width, canvas.height);
+    const load = async function (filename) {
+        const response = await fetch(filename);
+        return await response.arrayBuffer();
+    };
 
+    const parse = function (buffer) {
+        const array = new BigUint64Array(buffer);
+        console.assert(array.length % 3 === 0, "Invalid buffer length.");
+
+        const checkpoints = [];
+        const events = [];
+        for (let i = 0; i < array.length; i += 3) {
+            const type = array[i];
+            const size = array[i + 1];
+            const pointer = array[i + 2];
+
+            if (type === Event.Allocation) {
+                events.push({ "type": type, "size": size, "pointer": pointer });
+            } else if (type === Event.Free) {
+                events.push({ "type": type, "pointer": pointer });
+            } else if (type === Event.Checkpoint) {
+                checkpoints.push(i - checkpoints.length);
+            } else {
+                console.error("Invalid event type.");
+            }
+        }
+
+        return { events, checkpoints };
+    };
+
+    // TODO Remove.
+    const getRandomColor = function () {
+        return "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+    };
+
+    const getNPages = function (events) {
+        const pages = new Set();
+        for (let event of events) {
+            if (event.type === Event.Allocation) {
+                const page = event.pointer >> 12n;
+                pages.add(page);
+            }
+        }
+        return pages.size;
     }
 
-    window.addEventListener('resize', render, false);
+    const buffer = await load("heapmap.log");
+    const { events, checkpoints } = parse(buffer);
+    const n = getNPages(events);
+    console.log(events);
+    console.log(checkpoints);
+    console.log(n);
 
+    const render = function () {
+        const windowWidth = window.innerWidth - 50;
+        const windowHeight = window.innerHeight - 50;
+        const squaresPerRow = Math.ceil(Math.sqrt(n));
+        const nRows = Math.ceil(n / squaresPerRow);
+        const squareSize = Math.min(windowWidth / squaresPerRow, windowHeight / nRows);
+
+        canvas.width = squaresPerRow * squareSize;
+        canvas.height = nRows * squareSize;
+
+        for (let i = 0; i < nRows; ++i) {
+            for (let j = 0; j < squaresPerRow; ++j) {
+                const x = j * squareSize;
+                const y = i * squareSize;
+                const color = getRandomColor();
+                context.fillStyle = color;
+                context.fillRect(x, y, squareSize, squareSize);
+            }
+        }
+    };
+
+    window.addEventListener("resize", render, false);
     render();
 })();
